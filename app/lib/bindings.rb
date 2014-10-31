7285dde8500219fcd90bb91ece0b71e9b55e508b
Bruno Casali <brunoocasali@gmail.com>
2014-10-31 15:48:53 -0200

    remove the message: block (3 levels) in <top (required)>: [DEPRECATION] #[:user] is deprecated. Use #user to fetch the value.

--- app/lib/bindings.rb
+++ app/lib/bindings.rb
@@ -4,19 +4,19 @@ Pakyow::App.bindings do
   scope :tweet do
   
   	binding :avatar do
-  	  { src: bindable[:user][:profile_image_url] }
+  	  { src: bindable.user.profile_image_url }
   	end
   
   	binding :user do
-		  bindable[:user][:nickname]
+		  bindable.user.name
 		end
 		
 		binding :show do
-		  { href: router.group(:tweet).path(:show, tweet_id: bindable[:id]) }
+		  { href: router.group(:tweet).path(:show, tweet_id: bindable.id) }
 		end
 		
 		binding :twitter do
-	  	{ href: "http://twitter.com/#{bindable[:user][:screen_name]}/status/#{bindable[:id]}" }
+	  	{ href: "http://twitter.com/#{bindable.user.screen_name}/status/#{bindable.id}" }
 		end
 		
 	end
